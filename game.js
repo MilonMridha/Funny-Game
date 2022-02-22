a3b15cca4fdece0428684c1da636bd6573760f21
MilonMridha <milonmridha570@gmail.com>
2022-02-22 20:51:26 +0600

    added funny game

---
+++ game.js
@@ -0,0 +1,22 @@
+document.getElementById('btn').addEventListener('click', function(){
+    const input = document.getElementById('input');
+     const inputNumber = parseInt(input.value);
+     if(input.value == '' || input.value == NaN || input.value > 10){
+       alert('Input A Number then Cheek')
+   } else{
+    const getInput = document.getElementById('inputed');
+    getInput.innerText = inputNumber;
+    const random = document.getElementById('random');
+    random.innerText = Math.round(Math.random()* 10);
+   }
+   input.value = '';
+   if(inputNumber == parseInt(random.innerText)){
+       alert('WOW YOU ARE A WINNER AND YOU GOT A IPHONE 12 PRO MAX')
+   }
+   else{
+       document.getElementById('fail').style.display = 'block';
+   }
+});
+function reload() {
+    window.location.reload();
+  }
